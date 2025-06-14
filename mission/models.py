e13fbab2f25f3237bcec36069ac60db1a9644a0b
vllynnyk <magnetto54@gmail.com>
2025-06-14 15:05:17 +0200

    Create Task model

--- mission/models.py
+++ mission/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    content = models.TextField(blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    is_completed = models.BooleanField(default=False)
+    tags = models.ManyToManyField("Tag", related_name="tasks")
+
+    class Meta:
+        ordering = ["is_completed"]
+
+    def __str__(self):
+        return f"{self.content} - {self.created_date} - {self.is_completed}"
