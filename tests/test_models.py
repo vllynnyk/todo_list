b8f68e7f75346d9eb81a6d5ab2b230166409ee73
vllynnyk <magnetto54@gmail.com>
2025-06-14 15:13:34 +0200

    Add test for __str__ method of Task model

--- tests/test_models.py
+++ tests/test_models.py
@@ -13,3 +13,11 @@ class TaskTest(TestCase):
         self.assertEqual(task.content, "load the dishwasher")
         self.assertFalse(task.is_completed)
         self.assertIsNotNone(task.created_date)
+
+    def test_task_str(self):
+        task = Task.objects.create(
+            content="load the dishwasher",
+            is_completed=False,
+        )
+        self.assertEqual(str(task),
+                         f"{task.content} - {task.created_date} - {task.is_completed}")
