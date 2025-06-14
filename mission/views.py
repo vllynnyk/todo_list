13ca809fa7632d812f869d7e8154ad0bfa662def
vllynnyk <magnetto54@gmail.com>
2025-06-14 15:36:53 +0200

    Create TaskListView for Task model

--- mission/views.py
+++ mission/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from mission.models import Task
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    paginate_by = 10
