6f5ed5ac6145e7b559f4a6d9b7a533c18925cd98
vllynnyk <magnetto54@gmail.com>
2025-06-14 15:37:23 +0200

    Create TaskCreateView for Task model

--- mission/views.py
+++ mission/views.py
@@ -7,3 +7,9 @@ from mission.models import Task
 class TaskListView(generic.ListView):
     model = Task
     paginate_by = 10
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("mission:task_list")
